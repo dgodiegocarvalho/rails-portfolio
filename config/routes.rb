59380401ed562158bebe55f2badcc09074c92c70
Diego <dgo.diegocarvalho@gmail.com>
2020-11-10 16:56:00 -0300

    basic auth using email and pwd

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   get 'about', to: 'static_pages#about'
-	get 'contact', to: 'static_pages#contact'
+  get 'contact', to: 'static_pages#contact'
+
+  get 'signin', to: 'sessions#new'
+  post 'signin', to: 'sessions#create'
+  delete 'signout', to: 'sessions#destroy'
+  
+  resources :users
 end
